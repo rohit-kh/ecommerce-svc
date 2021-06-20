06baae170e2c26c08021132fa1df5ed18044839b
rohit-kh <rohitkhatri.in@gmail.com>
2021-06-20 23:36:59 +0530

    Set headers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const productRouter = require('./routers/product.router')
 const app = express()
 const port = process.env.PORT
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
+
 app.use(express.json())
 app.use(auth)
 app.use('/users', userRouter)
